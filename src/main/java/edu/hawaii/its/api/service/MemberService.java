5567061c4b4b38bc517b7637601f106c92e6e1ae
zakgilbert <gilbertz@hawaii.edu>
2022-11-09 04:42:20 -1000

    Add some comments to help.

--- src/main/java/edu/hawaii/its/api/service/MemberService.java
+++ src/main/java/edu/hawaii/its/api/service/MemberService.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("memberService")
+
+/**
+ * MemberService checks if a UH affiliate is a member of a group or grouping.
+ */
 public class MemberService {
     @Value("${groupings.api.grouping_admins}")
     private String GROUPING_ADMINS;
@@ -14,30 +18,36 @@ public class MemberService {
         HasMemberCommand hasMemberCommand = new HasMemberCommand(GROUPING_ADMINS, uhIdentifier);
         return hasMemberCommand.execute().getResultCode().equals("IS_MEMBER");
     }
+   // public GroupingHasMemberResult isAdminResult(String uhIdentifier);
 
     public boolean isMember(String groupPath, String uhIdentifier) {
         return false;
     }
+    // public GroupingHasMemberResult isMemberResult(String uhIdentifier);
 
     public boolean isIncludeMember(String groupingPath, String uhIdentifier) {
         return false;
     }
+    // public GroupingHasMemberResult isIncludeMemberResult(String uhIdentifier);
 
     public boolean isExcludeMember(String groupingPath, String uhIdentifier) {
         return false;
     }
+    // public GroupingHasMemberResult isExcludeMemberResult(String uhIdentifier);
 
     public boolean isOwner(String groupingPath, String uhIdentifier) {
         return false;
     }
-
-
+    // public GroupingHasMemberResult isOwnerResult(String uhIdentifier);
     /*
-    public GroupingsHasMemberResults hasAdmins(List<String> uhIdentifiers);
-    public GroupingsHasMemberResults hasOwners(String groupingPath, List<String> uhIdentifiers);
-    public GroupingsHasMemberResults hasMembers(String groupPath, List<String> uhIdentifiers);
-    public GroupingsHasMemberResults hasIncludeMembers(String groupPath, List<String> uhIdentifiers);
-    public GroupingsHasMemberResults hasExcludeMembers(String groupPath, List<String> uhIdentifiers);
+    public GroupingHasMembersResult hasAdmins(List<String> uhIdentifiers);
+    public GroupingHasMembersResult hasOwners(String groupingPath, List<String> uhIdentifiers);
+    public GroupingHasMembersResult hasMembers(String groupPath, List<String> uhIdentifiers);
+    public GroupingHasMembersResult hasIncludeMembers(String groupPath, List<String> uhIdentifiers);
+    public GroupingHasMembersResult hasExcludeMembers(String groupPath, List<String> uhIdentifiers);
      */
-
+    // Todo: GroupingHasMemberResult and GroupingsHasMembersResult still need to be implemented.
+    /////// Look at GroupingsAddResult and GroupingsAddResults as an example.
+    /////// GroupingHasMemberResult and GroupingsHasMembersResult will allow data results about members to be
+    /////// Returned to the UI>
 }
